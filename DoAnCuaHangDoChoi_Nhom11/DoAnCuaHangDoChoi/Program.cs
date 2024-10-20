53f0480f7fa13a31ea4c7dde3b863f5febffe74a
hoangtien94huee@gmail.com <hoangtien94huee@gmail.com>
2024-10-20 21:25:27 +0700

    TIEN

--- DoAnCuaHangDoChoi_Nhom11/DoAnCuaHangDoChoi/Program.cs
+++ DoAnCuaHangDoChoi_Nhom11/DoAnCuaHangDoChoi/Program.cs
@@ -16,7 +16,7 @@ namespace DoAnCuaHangDoChoi
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLich());
+            Application.Run(new frmDoChoi());
         }
     }
 }
