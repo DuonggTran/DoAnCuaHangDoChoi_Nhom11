25866fdb6a8e2107187a6af349f0863c0e22f289
NguyenNgocThaoo <nguyenngocthao1502@gmail.com>
2024-10-20 16:26:15 +0700

    Create forms

--- DoAnCuaHangDoChoi_Nhom11/DataAccessLayer/DAL.cs
+++ DoAnCuaHangDoChoi_Nhom11/DataAccessLayer/DAL.cs
@@ -14,7 +14,7 @@ namespace DataAccessLayer
         SqlCommand cmd = null;
         SqlDataAdapter adp = null;
 
-        string strConnect = "DESKTOP-R8CP7IB;Initial Catalog=KetNoiCSDL;Integrated Security=True;Encrypt=True";
+        string strConnect = "DESKTOP-NC8HSHQ;Initial Catalog=KetNoiCSDL;Integrated Security=True;Encrypt=True";
 
         public DAL()
         {
