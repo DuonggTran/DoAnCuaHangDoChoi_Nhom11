c5774887e62a108b4aa1962f724895247d00da2c
vulong03 <longvu.2043@gmail.com>
2024-10-20 18:09:48 +0700

    them class

--- DoAnCuaHangDoChoi_Nhom11/BusinessLogicLayer/DBHoaDonNhap.cs
+++ DoAnCuaHangDoChoi_Nhom11/BusinessLogicLayer/DBHoaDonNhap.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DataAccessLayer;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,43 @@ namespace BusinessLogicLayer
 {
     internal class DBHoaDonNhap
     {
+        DAL db = null;
+        public DBHoaDonNhap()
+        {
+            db = new DAL();
+        }
+        //load ds hóa đơn nhập 
+        public DataSet LayHoaDonNhap()
+        {
+            return db.ExecuteQueryDataSet("SELECT * FROM UDF_LayHoaDonNhap()", CommandType.Text);
+        }
+        //Thêm hóa đơn nhập
+        public bool ThemHoaDonNhap(ref string err, string mahoadonnhap, string ngaynhap, string manhanvien)
+        {
+            return db.MyExecuteNonQuery("USP_ThemHoaDonNhap", CommandType.StoredProcedure, ref err,
+                new SqlParameter("@ma_hoa_don_nhap", mahoadonnhap),
+                new SqlParameter("@ngay_nhap", DateTime.Parse(ngaynhap)),
+                new SqlParameter("@ma_nhan_vien", manhanvien));
+        }
+        //Cập nhật hoá đơn nhập
+        public bool CapNhatHoaDonNhap(ref string err, string mahoadonnhap, string ngaynhap, string manhanvien)
+        {
+            return db.MyExecuteNonQuery("USP_CapNhatHoaDonNhap", CommandType.StoredProcedure, ref err,
+                new SqlParameter("@mahoadonnhap", mahoadonnhap),
+                new SqlParameter("@ngaynhap", DateTime.Parse(ngaynhap)),
+                new SqlParameter("@manhanvien", manhanvien));
+        }
+        //Xoá hoá đơn nhập
+        public bool XoaHoaDonNhap(ref string err, string mahoadonnhap)
+        {
+            return db.MyExecuteNonQuery("USP_XoaHoaDonNhap", CommandType.StoredProcedure, ref err,
+                new SqlParameter("@MaHoaDonNhap", mahoadonnhap));
+        }
+
+        // Tìm kiếm hoá đơn nhập: UDF_TimHoaDonNhap
+        public DataSet TimHoaDonNhap(string mahoadonnhap)
+        {
+            return db.ExecuteQueryDataSet("SELECT * FROM UDF_TimHoaDonNhap(@mahoadonnhap)", CommandType.Text);
+        }
     }
 }
